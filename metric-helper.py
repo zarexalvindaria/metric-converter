21f0341f1ec5f6608fd193dfed9752092983fd15
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2021-07-23 21:27:23 +0800

    created the root window

---
+++ metric-helper.py
@@ -0,0 +1,12 @@
+#Metric Helper Project
+import tkinter as tk
+
+# Define window
+root = tk.Tk()
+root.title("Metric Helper")
+root.iconbitmap("ruler.ico")
+root.resizable(0,0)
+
+#Run the root window
+root.mainloop()
+

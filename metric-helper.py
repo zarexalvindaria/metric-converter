de5e15c4f2ca771f56d3d6ff1a7748d139f286ac
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2021-07-23 22:43:01 +0800

    added the convert button

--- metric-helper.py
+++ metric-helper.py
@@ -44,6 +44,13 @@ input_dropdown.grid(row=1, column=0)
 to_label.grid(row=1, column=1)
 output_dropdown.grid(row=1, column=2)
 
+input_choice.set('base value')
+output_choice.set('base value')
+
+#Create a conversion button
+convert_button = tk.Button(root, text="Convert", font=field_font, bg=button_color)
+convert_button.grid(row=2, column=0, columnspan=3)
+
 #Run the root window
 root.mainloop()
 
